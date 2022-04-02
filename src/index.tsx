4342a60495e434256a218676c4559c5ca3074c51
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2022-04-02 12:15:27 +0300

    init: start

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
